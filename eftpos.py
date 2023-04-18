f31bf518eed33b99b6e1b348ecfc21e86c7c01e4
Xbox One <sondad2008@gmail.com>
2023-04-18 14:43:15 -0400

    More environment variable command fixing

--- eftpos.py
+++ eftpos.py
@@ -10,7 +10,7 @@ if environ.get('BUSINESS_ID'):
     business_id = environ['BUSINESS_ID']
 else:
     business_id = input("What is your business's account ID at La Banque? ")
-    system("SET BUSINESS_ID = " + business_id + " && EXPORT BUSINESS_ID")
+    system("export BUSINESS_ID='" + business_id + "'")
 
 root = Tk()
 
